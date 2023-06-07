9f6e6136fc3ac434dd0ba89de3643340c04f21b3
shankthatank <shankaracarson@gmail.com>
2023-06-07 12:38:10 -0700

    current build

--- Assets/Scripts/Objects/ObjectInteract.cs
+++ Assets/Scripts/Objects/ObjectInteract.cs
@@ -9,40 +9,86 @@ public class ObjectInteract : MonoBehaviour
     public GameObject player;
     bool withinRange = false;
     LayerMask mask;
-    public string textVal;
+    public string blackTextVal;
+    public string asianTextVal;
+    public string indianTextVal;
+    public string nativeTextVal;
+    public string latinoTextVal;
+    public string middleTextVal;
+    public GameObject blackObject;
+    public GameObject asianObject;
+    public GameObject indianObject;
+    public GameObject nativeObject;
+    public GameObject latinoObject;
+    public GameObject middleObject;
     public TextMeshProUGUI textComponent;
     bool textOpen = false;
+    bool totalRange = false;
+    int curText = 7;
+    List<GameObject> objects = new List<GameObject>();
+    List<string> texts = new List<string>();
 
     // Start is called before the first frame update
     void Start()
     {
-      mask = LayerMask.GetMask("Player");
-        textVal = "Fortnite Burger 2019";
-       
+      mask = LayerMask.GetMask("Object");
+        objects.Add(blackObject);
+        objects.Add(asianObject);
+        objects.Add(indianObject);
+        objects.Add(nativeObject);
+        objects.Add(latinoObject);
+        objects.Add(middleObject);
+
+        texts.Add(blackTextVal);
+        texts.Add(asianTextVal);
+        texts.Add(indianTextVal);
+        texts.Add(nativeTextVal);
+        texts.Add(latinoTextVal);
+        texts.Add(middleTextVal);
+        texts.Add("Press E to open");
+        texts.Add("Find sculptures and learn about them");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.DrawRay(transform.position, (player.transform.position - transform.position).normalized * 3f, Color.blue);
-        if(Physics.Raycast(transform.position, (player.transform.position - transform.position).normalized, 3f, mask))
+        totalRange = false;
+        for (int i = 0; i < objects.Count; i++)
         {
-            withinRange = true;
+            Debug.DrawRay(player.transform.position, (objects[i].transform.position - player.transform.position).normalized * 3f, Color.blue);
+            if (Physics.Raycast(player.transform.position, (objects[i].transform.position - player.transform.position).normalized, 3f, mask))
+            {
+                Debug.Log(i);
+                withinRange = true;
+                totalRange = true;
+            }
+            else
+            {
+                withinRange = false;
+            }
+            if (withinRange && Input.GetKeyDown(KeyCode.E))
+            {
+                textOpen = !textOpen;
+            }
+            if (withinRange && !textOpen)
+            {
+                curText = 6;
+            }
+            else if (withinRange && textOpen)
+            {
+                curText = i;
+            }
+
+        }
+        if (totalRange)
+        {
+            textComponent.text = texts[curText];
         }
         else
         {
-            withinRange = false;
+            textComponent.text = texts[7];
         }
-        if(withinRange && Input.GetKeyDown(KeyCode.E))
-        {
-            textOpen = !textOpen;
-        }
-        if(withinRange && !textOpen)
-        {
-            textComponent.text = "Press E to open";
-        } else if(withinRange && textOpen) {
-            textComponent.text = textVal;
-        }
-
+                
     }
 }
